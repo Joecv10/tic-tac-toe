25ecd99c771f21372b451d351696679648cc2da7
Joe Concha Vasquez <joeconchavasquez@gmail.com>
2024-05-08 10:49:30 -0500

    The game now allows to take turns and declares a winner

--- tictactoe/src/App.js
+++ tictactoe/src/App.js
@@ -8,6 +8,9 @@ const App = () => {
   const [xisNext, setXisNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
   const handleClick = (i) => {
+    if (squares[i] || calculateWinner(squares)) {
+      return;
+    }
     const nextSquare = squares.slice();
     if (xisNext) {
       nextSquare[i] = "X";
@@ -17,8 +20,43 @@ const App = () => {
     setSquares(nextSquare);
     setXisNext(!xisNext);
   };
+  const calculateWinner = (squares) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
+    return null;
+  };
+
+  const winner = calculateWinner(squares);
+  let status;
+
+  if (winner) {
+    status = `Winner: ${winner}`;
+  } else {
+    status = `Next player: ${xisNext ? "X" : "O"}`;
+  }
+
   return (
     <div>
+      <p className="status">{status}</p>
       <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
