73955580e91168619ecb55c38267488e7ecd6e27
Joe Concha Vasquez <joeconchavasquez@gmail.com>
2024-05-10 11:21:34 -0500

    Finalized implementing the time travel function

--- tictactoe/src/Game.js
+++ tictactoe/src/Game.js
@@ -4,26 +4,30 @@ import "./Square";
 import App from "./App";
 
 const Game = () => {
-  const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
-  const currentSquares = history[history.length - 1];
+  const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
+  const currentSquares = history[currentMove];
 
   const handlePlay = (nextSquares) => {
-    setHistory([...history, nextSquares]);
-    setXIsNext(!xIsNext);
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
   };
 
-  const jumpTo = (nexMove) => {};
+  const jumpTo = (nextMove) => {
+    setCurrentMove(nextMove);
+  };
 
   const moves = history.map((squares, move) => {
     let description;
     if (move > 0) {
-      description = "Go to move #";
+      description = `Go to move #${move}`;
     } else {
       description = "Go to game start";
     }
     return (
-      <li>
+      <li key={move}>
         <button onClick={() => jumpTo(move)}>{description}</button>
       </li>
     );
